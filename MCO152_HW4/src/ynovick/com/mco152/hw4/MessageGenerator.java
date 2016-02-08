b105c7156a2dfb96cda3304db33162056ab0a5e0
Student <Student@10.0.2.15>
2016-02-08 18:29:39 -0500

    Adding files

---
+++ MCO152_HW4/src/ynovick/com/mco152/hw4/MessageGenerator.java
@@ -0,0 +1,8 @@
+package ynovick.com.mco152.hw4;
+
+public class MessageGenerator {
+	public static String getMessage()
+	{
+		return "Hello World!";
+	}
+}
---
+++ MCO152_HW4/src/ynovick/com/mco152/hw4/Program.java
@@ -0,0 +1,11 @@
+package ynovick.com.mco152.hw4;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(MessageGenerator.getMessage());
+	
+	}
+
+}
