ade72d64885fe757aee04755ebde06c37e990138
Student <Student@10.0.2.15>
2016-02-08 18:34:15 -0500

    Added comment with todo item so I could be lazy

--- MCO152_HW4/src/ynovick/com/mco152/hw4/MessageGenerator.java
+++ MCO152_HW4/src/ynovick/com/mco152/hw4/MessageGenerator.java
@@ -3,6 +3,10 @@ package ynovick.com.mco152.hw4;
 public class MessageGenerator {
 	public static String getMessage()
 	{
+		// This is just a dummy message, eventually
+		// we want this method to generate different
+		// messages with some sort of logic
+		// TODO Get back to this
 		return "Hello World!";
 	}
 }
